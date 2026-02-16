e74f1e8ca9e54392f732c96d76ef01f00f4c3004
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2026-02-16 15:33:46 +0600

    updated

--- app/Http/Controllers/Admin/DefinitionController.php
+++ app/Http/Controllers/Admin/DefinitionController.php
@@ -38,9 +38,9 @@ class DefinitionController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): Response
     {
-        //
+        return Inertia::render('admin/word-definition/create');
     }
 
     /**
